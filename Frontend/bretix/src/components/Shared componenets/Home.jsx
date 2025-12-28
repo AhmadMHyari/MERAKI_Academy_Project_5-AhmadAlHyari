37c349f98ff74cea09368ff1aebfb21dc3e707cd
AhmadMAlhyari <ahmad.m.alhiyari@gmail.com>
2025-12-28 22:43:04 +0300

    home Frontend images preview

--- Frontend/bretix/src/components/Shared componenets/Home.jsx
+++ Frontend/bretix/src/components/Shared componenets/Home.jsx
@@ -1,8 +1,27 @@
-const Home = ()=>{
-    return (
-        <div>
-            Home Section
-        </div>
-    )
-}
-export default Home
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Home = () => {
+  const navigate = useNavigate()
+    const [top10Products,setTop10Products] = useState([])
+    useEffect(()=>{
+        axios.get("http://localhost:5000/products/top10")
+        .then((res)=>{setTop10Products(res.data.result)})
+        .catch((err)=>{console.log(err);
+        })
+    },[])
+    
+    const showTop10 = top10Products.map((product)=>{
+      return <div key={product.id}>
+        <img src={product.imgsrc} alt={product.title} onClick={()=>{navigate(`/product/${product.id}`)}} /><br />
+        <h3>{product.title}</h3>
+        <span>${product.price}</span> <button>+Add to Cart</button>
+      </div>
+    })
+  return <div> 
+    {showTop10}
+  </div>;
+};
+export default Home;
